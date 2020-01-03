bdbb8cee5a4c2bfbd579dc6a8d39b770babf2df1
dipinforce <dipinpta12@gmail.com>
2020-01-03 11:09:35 +0530

    newfinal

---
+++ app.js
@@ -0,0 +1,130 @@
+var express=require('express')
+var mongoose =require('mongoose')
+var bodyParser=require('body-parser')
+
+
+const Productmodel=mongoose.model("productdetails",
+
+{
+    productname:String,
+    itemcode:String,
+    quality:String,
+    sellername:String,
+    price:String
+    
+
+
+}
+)
+mongoose.connect("mongodb+srv://dipinforce:forcepandalam@cluster0-sspts.mongodb.net/test?retryWrites=true&w=majority")
+
+
+ var app=express();
+
+ app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+// For CORS,Pgm Line no 12 to 29
+    app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*' );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+app.get('/',(req,res)=>{
+res.send('hello');
+});
+
+app.post('/Product',function(req,res){
+
+ var productname=req.body.productname
+ var itemcode=req.body.itemcode
+ var quality=req.body.quality
+ var sellername=req.body.sellername
+ var price=req.body.price
+ 
+
+   var Product=new Productmodel(req.body)
+
+   Product.save( (error,data)=>{
+       if(error){
+           throw error;
+       }
+           else{
+               res.send(data);
+       }
+    })
+})
+
+app.get('/viewall',(req,res)=>{
+
+var result=Productmodel.find( (error,data)=>{
+     if(error){
+     throw error;
+     }
+     else{
+         res.send(data);
+     }
+    })
+    })
+
+app.post('/update',(req,res)=>{
+var productname=req.body.productname;
+var itemcode=req.body.itemcode;
+var quality=req.body.quality;
+var sellername=req.body.sellername;
+var price=req.body.price;
+var result=Productmodel.findByIdAndUpdate(id,{"productname":productname,"itemcode":itemcode,"quality":quality,"sellername":sellername,"price":price},(error,data)=>{
+
+    if(error){
+        throw error;
+}
+else{
+    res.send("succesfully Updated" + data);
+}
+})
+})
+app.post('/remove',(req,res)=>{
+
+    
+    var result=Productmodel.findByIdAndRemove( id,(error,data)=>{
+        if(error){
+            newFunction(error)
+        }
+        else{
+            res.send("succesfully removed" + data);
+        }
+    })
+})
+app.post('/search',(req,res)=>{
+    var itemcode=req.body.itemcode;
+    var result=Productmodel.find({"itemcode":itemcode},(error,data)=>{
+if(error){
+    throw error;
+}
+else{
+
+res.send("succesfully searched" + data);
+}
+    })
+})
+
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('server started')
+})
+
